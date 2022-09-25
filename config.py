75dc0465d357492a36b1becc0b607d207708ac71
AlexxGorr <48887077+AlexxGorr@users.noreply.github.com>
2022-09-25 17:52:05 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+
+TOKEN = '5487683906:AAED5AuLe4wU14cR2THvtQpXCvh5T2y5e9I'
+
+keys = {
+        'биткоин': 'BTC',
+        'эфириум': 'ETH',
+        'доллар': 'USD',
+        'рубль': 'RUB',
+        'евро': 'EUR',
+        'юань': 'CNY'
+        }
